379a68dfb818586605d0acee6ff46edb18bb869c
AlmuHusan <38337359+AlmuHusan@users.noreply.github.com>
2019-07-10 13:57:11 -0400

    Redux Thunk Complete

--- redux_proj/reduxthunk/src/reducers/index.js
+++ redux_proj/reduxthunk/src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux'
 import postReducer from './postsReducer';
+import usersReducer from './usersReducer';
 export default combineReducers({
-    post:postReducer
-
+    posts:postReducer,
+    users:usersReducer
 
 });
